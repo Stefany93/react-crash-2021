7adaee8cddf41d27831f3df3f4544a4ce76ccf25
Stefany Newman <stefany.newman93@gmail.com>
2021-10-13 15:01:07 -0500

    Fixed button / heading styling and positioning

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,16 +6,20 @@ const Task = ({ task, onDelete, onToggle }) => {
       className={`task ${task.reminder && 'reminder'}`}
       onDoubleClick={() => onToggle(task.id)}
     >
+      <div className="heading-button-container">
       <h3>
         {task.text}{' '}
+      </h3>
+      
         <button aria-label="Delete task" className="close-button">
           <FaTimes
             style={{ color: 'red', cursor: 'pointer' }}
             onClick={() => onDelete(task.id)}
           />
         </button>
-      </h3>
+        </div>
       <p>{task.day}</p>
+      
     </div>
   )
 }
